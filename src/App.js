36a72ffd466e794891e8d6460be2fecec599e750
Shedrack Sunday <Hannania002@gmail.com>
2022-11-08 14:29:35 +0100

    Add app comonent file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import {
-  Route,
-  Routes,
-} from 'react-router-dom';
-import Books from './pages/Books';
-import Categories from './pages/Categories';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Header from './components/Header';
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="categories" element={<Categories />} />
-      </Routes>
-    </>
+    <div
+      className="App"
+      style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', height: '100%' }}
+    >
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/*" element={<div>Dont exist</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
+
+export default App;
