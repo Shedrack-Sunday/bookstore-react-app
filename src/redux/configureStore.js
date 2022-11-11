b97032ba57fb0d299e618a84a89734b6d16bac86
Shedrack Sunday <Hannania002@gmail.com>
2022-11-11 09:10:08 +0100

    Reducer configuration changes

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
-
-export const Store = configureStore({ reducer: booksReducer });
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
