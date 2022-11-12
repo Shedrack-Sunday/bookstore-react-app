fd8ff074d868f81ca729466ae63931ca332ffe48
Shedrack Sunday <Hannania002@gmail.com>
2022-11-12 09:00:20 +0100

    Refator actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,122 +4,65 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const GET_CURRENT_API = 'bookStore/books/GET_CURRENT_API';
-const GET_CURRENT_API_SUCCESS = 'bookStore/books/GET_CURRENT_API_SUCCESS';
-const GET_CURRENT_API_FAILURE = 'bookStore/books/GET_CURRENT_API_FAILURE';
 const API = `
-  https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0ePu6tv5E4TBgiHo9yos/books`;
+  https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0ePu6tv5E4TBgiHo9yos/books/`;
 
-const initialState = {
-  status: 'Idle',
-  books: {},
-};
+const initialState = [];
 
-//  CreatAyncThunk Implimentation
- /* const addbk = createAsyncThunk(ADDBOOK, async (obj) => {
-  const url =
-    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0ePu6tv5E4TBgiHo9yos/books';
-  await fetch(url, {
-    method: 'POST',
-    headers: {
-      'content-Type': 'application/json',
-    },
-    body: JSON.stringify(obj),
-  });
-});
-
- const getbook = createAsyncThunk(GETBOOK, async () => {
-  const url =
-    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0ePu6tv5E4TBgiHo9yos/books';
-  const response = await fetch(url, {
+export const getApi = createAsyncThunk(GET_CURRENT_API, async () => {
+  const result = await fetch(API, {
     method: 'GET',
     headers: {
-      'content-Type': 'application/json',
+      'Content-type': 'application/json',
     },
   });
-  const result = await response.json();
+  const response = result.json();
+  return response;
 });
 
- const remBook = createAsyncThunk(REMBOOK, async (id) => {
-  const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0ePu6tv5E4TBgiHo9yos/books${id}`;
-  await fetch(url, {
+export const removeBook = createAsyncThunk(REMOVE_BOOK, (payload) => {
+  fetch(`${API}${payload}`, {
     method: 'DELETE',
     headers: {
-      'content-Type': 'application/json',
+      'Content-type': 'application/json',
     },
-    body: JSON.stringify({ item_id: id }),
+    body: JSON.stringify({
+      item_id: payload,
+    }),
   });
-}); */
+  return payload;
+});
 
-export const getApi = async (dispatch) => {
-  dispatch({ type: GET_CURRENT_API });
-  const books = await fetch(API).then((res) => res.json());
-  return dispatch({ type: GET_CURRENT_API_SUCCESS, books });
-};
-
-export const removeBook = (payload) => async (dispatch) => {
-  const response = await fetch(API + payload, {
-    method: 'DELETE',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-  }).then((res) => res.status);
-  const result = response === 201 ? 'Book removed' : 'Fail to remove';
-  dispatch(getApi);
-  return dispatch({ type: REMOVE_BOOK, result });
-};
-
-export const addBook = (newBook) => async (dispatch) => {
-  const response = await fetch(API, {
+export const addBook = createAsyncThunk(ADD_BOOK, (newBook) => {
+  fetch(API, {
     method: 'POST',
-    body: JSON.stringify(newBook),
     headers: {
-      'Content-Type': 'application/json',
+      'Content-type': 'application/json',
     },
-  }).then((res) => res.status);
-  const result = response === 201 ? 'Book adding' : 'Fail to post';
-  dispatch(getApi);
-  return dispatch({ type: ADD_BOOK, result });
-};
+    body: JSON.stringify(newBook),
+  });
+  return newBook;
+});
 
 /* eslint-disable-next-line */
 const reducer = (state = initialState, action) => {
+  const newArray = [];
   switch (action.type) {
-    case ADD_BOOK:
-      return {
-        ...state,
-        status: action.result,
-      };
-    case REMOVE_BOOK: {
-      if (action.response === 201) {
-        return {
-          ...state,
-          status: 'Book Remove',
-        };
-      }
-      return state;
+    case `${ADD_BOOK}/fulfilled`:
+      return [...state, action.payload];
+    case `${REMOVE_BOOK}/fulfilled`: {
+      return state.filter((book) => book.item_id !== action.payload);
     }
-    case GET_CURRENT_API:
-      return {
-        ...state,
-        status: 'Loading',
-      };
-    case GET_CURRENT_API_SUCCESS:
-      return {
-        books: action.books,
-        status: 'SUCCESS',
-      };
-    case GET_CURRENT_API_FAILURE:
-      return {
-        ...state,
-        status: 'ERROR',
-      };
+    case `${GET_CURRENT_API}/fulfilled`:
+      Object.keys(action.payload).forEach((element) => {
+        const book = action.payload[element][0];
+        book.item_id = element;
+        newArray.push(book);
+      });
+      return newArray;
     default:
       return state;
   }
 };
-
-export const selectBooks = (state) => state.booksReducer.books;
 
 export default reducer;
